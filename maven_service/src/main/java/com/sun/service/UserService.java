d0559ced2590020b8bbe85dbf0bf4a2643b6de2c
sunpeng <sunpeng1@souche.com>
2017-03-31 15:09:50 +0800

    init

---
+++ maven_service/src/main/java/com/sun/service/UserService.java
@@ -0,0 +1,8 @@
+package com.sun.service;
+
+/**
+ * Created by sunpeng on 2017/3/31.
+ */
+public interface UserService {
+    void getUser();
+}
---
+++ maven_web/src/main/java/com/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.service.impl;
+
+import com.sun.service.UserService;
+
+/**
+ * Created by sunpeng on 2017/3/31.
+ */
+public class UserServiceImpl implements UserService{
+    public void getUser() {
+
+    }
+}
